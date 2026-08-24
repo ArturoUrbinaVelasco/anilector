e3c188ab37220f8c57162c62dd07b264c5457edf
Arturo Urbina <arturo.urbina@biossmann.com>
2026-08-24 17:47:27 -0600

    v1.3: sesion con Google y sincronizacion en Drive

--- js/config.js
+++ js/config.js
@@ -6,4 +6,4 @@
    Guía paso a paso en el README, sección "Inicio de sesión con Google".
    Si se deja vacío, la app funciona igual pero sin sesión ni
    sincronización con Google Drive. */
-export const GOOGLE_CLIENT_ID = "";
+export const GOOGLE_CLIENT_ID = "458798326994-lt03dlrr87ee3i80pqmogb185fsl797h.apps.googleusercontent.com";
